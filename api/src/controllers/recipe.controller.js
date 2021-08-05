732c9acd1cb870e7ed16677fa83408b9795e9590
ElLoko97 <80711598+ElLoko97@users.noreply.github.com>
2021-08-05 12:29:53 -0300

    2

--- api/src/controllers/recipe.controller.js
+++ api/src/controllers/recipe.controller.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Recipe, Diet } = require('../db.js');
 const axios = require('axios');
+const { Op } = require('sequelize');
 const { API_KEY } = process.env;
 
 function getRecipes(req, res, next) {
--- api/src/routes/diets.js
+++ api/src/routes/diets.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getDiets } = require('../controllers/diet.controller.js');
+const { getDiets } = require('../controllers/diet.controller');
 
 router.get('/types', getDiets);
 
