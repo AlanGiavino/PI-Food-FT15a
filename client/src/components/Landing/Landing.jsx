ddad926fea4d03fdbf84c4ccf532146ccd6ff0ce
ElLoko97 <80711598+ElLoko97@users.noreply.github.com>
2021-11-13 18:31:15 -0300

    start deploy

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Landing.css'
 import landingImg from '../../img/Landing.jpg'
-import logo from '../../img/Logo Food.png'
+import logo from '../../img/LogoFood.png'
 
 export default function Landing() {
 	return (
 		<React.Fragment>
-		<div className='brand-text-landing'>
-		<img className='logo-container' src={logo}/>
-			<img className='img-container' src={landingImg}/>
+			<div className='brand-text-landing'>
+				<img className='img-container' src={landingImg} />
+				<img className='logo-container' src={logo} />
 				<Link to='/recipes'>
 					<button className='btn'>Bon Appetite!</button>
 				</Link>
-		</div>
+			</div>
 		</React.Fragment>
 	);
 }
